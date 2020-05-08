5494540c74222e5f784daa8dc51159108bf6c84c
Diego Baldani <diego.baldani@bv.com.br>
2020-05-08 17:00:54 -0300

    first commit

---
+++ pong/app/main.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import os
+
+
+app = Flask(__name__)
+namespace = os.getenv("namespace")
+worker_host = "ping" + namespace
+
+@app.route("/")
+def backend():
+    r = requests.get("http://"+worker_host)
+    worker = socket.gethostbyname(worker_host)
+    return "Ping Message: {}\nFrom: {}".format(r.content, worker)
+
+
+if __name__ == "__main__":
+    # Only for debugging while developing
+    app.run(host="0.0.0.0", debug=True, port=8082)
